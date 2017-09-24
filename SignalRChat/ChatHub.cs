964ae6f37e07ec417533f9b1e538eae03c55e308
vpantea <vpantea@gmail.com>
2017-09-24 22:10:37 +0300

    Add project files.

---
+++ SignalRChat/ChatHub.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+
+namespace SignalRChat
+{
+    [HubName("chatHub")]
+    public class ChatHub : Hub
+    {
+        public void Send(string name, string message)
+        {
+            // Call the broadcastMessage method to update clients.
+            Clients.All.broadcastMessage(name, message);
+        }
+    }
+}
